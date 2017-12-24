2f5bfe4d664f1cb815b85715c6bed0d3e97cdca4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-23 21:59:18 -0500

    initial

---
+++ src/error-snapshot-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const errorSnapshot = require('.')
+
+describe('error-snapshot', () => {
+  it('write this test', () => {
+    console.assert(errorSnapshot, 'should export something')
+  })
+})
